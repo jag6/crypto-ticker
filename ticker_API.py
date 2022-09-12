f0c47c4c5adc60ef4dac6b57520637798a4e3a52
Matt <matt@randomforestweb.com>
2022-09-13 00:01:10 +0900

    first commit

---
+++ ticker_API.py
@@ -0,0 +1,16 @@
+import cryptocompare
+import time
+
+while True:
+    priceBTC = cryptocompare.get_price('BTC', 'USD')
+    priceETH = cryptocompare.get_price('ETH', 'USD')
+    priceADA = cryptocompare.get_price('ADA', 'USD')
+    priceSOL = cryptocompare.get_price('SOL', 'USD')
+    priceDOT = cryptocompare.get_price('DOT', 'USD')
+    
+    prices = priceBTC, priceETH, priceADA, priceSOL, priceDOT
+    
+    for price in prices:
+        print(price)
+    
+    time.sleep(5)
---
+++ ticker_webscrape.py
@@ -0,0 +1,47 @@
+import os
+import time
+import threading
+import requests
+from bs4 import BeautifulSoup
+
+# get the data
+data = requests.get('https://coinmarketcap.com')
+
+# load data into bs4
+soup = BeautifulSoup(data.text, 'html.parser')
+
+coinboard = soup.find('table', { 'class': 'h7vnx2-2 czTsgW cmc-table' })
+tbody = coinboard.find('tbody')
+tr = tbody.find_all('tr')
+
+BTC = 'Bitcoin1'
+ETH = 'Ethereum2'
+ADA = 'Cardano'
+SOL = 'Solana'
+DOT = 'Polkadot'
+
+crypto = BTC, ETH, ADA, SOL, DOT
+
+
+def clear_data():
+    time.sleep(5)
+    os.system('clear')
+
+def get_data():
+	for tr in tbody:
+		name = tr.find_all('td')[2].text.strip()
+		price = tr.find_all('td')[3].text.strip()
+		if BTC in name:
+			print('BTC: ', price)
+		if ETH in name:
+			print('ETH: ', price)
+		if ADA in name:
+			print('ADA: ', price)
+		if SOL in name:
+			print('SOL: ', price)
+		if DOT in name:
+			print('DOT: ', price)
+
+while True:
+    clear_data()
+    get_data()
